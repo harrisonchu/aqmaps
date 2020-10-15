46dc58ba83a94a029d02186f58b8568eb509eee9
Kathryn Chu <kathryn.chu10@gmail.com>
2020-10-14 19:04:59 -0700

    Preload gh_disctricts data with aggregated accessibility data

--- frontend/src/actions/datasetActions.js
+++ frontend/src/actions/datasetActions.js
@@ -1,35 +1,9 @@
 import { addDataToMap } from 'kepler.gl/actions';
+import { processGeojson } from 'kepler.gl/processors';
 
-import defaultGeoJson from '../data/gh_districts.json';
+import defaultGeoJson from '../data/gh_districts_with_accessibility.geojson.json';
 
 export const fetchDataset = () => (dispatch) => {
-    const sampleTripData = {
-        fields: [
-          {name: 'tpep_pickup_datetime', format: 'YYYY-M-D H:m:s', type: 'timestamp'},
-          {name: 'pickup_longitude', format: '', type: 'real'},
-          {name: 'pickup_latitude', format: '', type: 'real'}
-        ],
-        rows: [
-          ['2015-01-15 19:05:39 +00:00', -73.99389648, 40.75011063],
-          ['2015-01-15 19:05:39 +00:00', -73.97642517, 40.73981094],
-          ['2015-01-15 19:05:40 +00:00', -73.96870422, 40.75424576],
-        ]
-       };
-       
-    //    const sampleConfig = {
-    //      visState: {
-    //        filters: [
-    //          {
-    //            id: 'me',
-    //            dataId: 'test_trip_data',
-    //            name: 'tpep_pickup_datetime',
-    //            type: 'timeRange',
-    //            enlarged: true
-    //          }
-    //        ]
-    //      }
-    //    }
-
     dispatch(
         addDataToMap({
             datasets: {
@@ -37,7 +11,7 @@ export const fetchDataset = () => (dispatch) => {
                     label: 'Districts of Ghana',
                     id: 'default_dataset_ghana'
                 },
-                data: sampleTripData
+                data: processGeojson(defaultGeoJson)
             },
             option: {
                 centerMap: true,

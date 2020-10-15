b84c6e84e9aa030bfd4f45fdf67020817fd4a44f
Kathryn Chu <kathryn.chu10@gmail.com>
2020-10-14 20:47:45 -0700

    Add country selection component to homepage

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Kepler from './Kepler/Kepler'
+import Kepler from './Kepler/Kepler';
+import CountrySelect from './CountrySelect/CountrySelect';
 
 import './App.scss';
 
 const App = () => (
   <div>
     <Switch>
-      <Route path="/" component={Kepler} />
+      <Route path="/map" component={Kepler} />
+      <Route exact path="/" component={CountrySelect} />
     </Switch>
   </div>
 );

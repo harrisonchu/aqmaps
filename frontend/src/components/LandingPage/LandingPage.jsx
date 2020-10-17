398df81a82de93b28d5e605caf2f9c484c686980
Kathryn Chu <kathryn.chu10@gmail.com>
2020-10-17 10:47:02 -0700

    More landing page updates

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -6,10 +6,20 @@ import './LandingPage.scss';
 class LandingPage extends React.Component {
     render() {
         return (
-            <div className='welcome-widget'>
-                <h1>Welcome to AqMaps!</h1>
-                <CountrySelect />
-            </div>
+            <div className='landing-page-container'>
+                <div className='left-pane'>
+                    <img className='hero-image'
+                        src='https://images.unsplash.com/photo-1564810138081-feb6b1ee8e33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+                    />
+                </div>
+                    <div className='right-pane'>
+                        <h1>Welcome to Aqmaps!</h1>
+                        <CountrySelect />
+                        <p>
+                            The Aquaya Institute is a non-profit research and consulting organization dedicated to improving health in the developing world. We deliver the knowledge and tools that are required to achieve universal access to safe water and sanitation.
+                        </p>
+                    </div>
+                </div>
         )
     }
 };

2d06d157c4b49cb4c4eed4fb0d612201087a8164
harrisonchu <hchu@lyft.com>
2020-10-17 12:43:09 -0700

    wip

--- compress_geojson.py
+++ compress_geojson.py
@@ -14,6 +14,8 @@ def _lower_resolution(list_of_coords, reduction_factor):
     return result
 
 def _lower_resolution_v2(list_of_coords, reduction_factor):
+    corners = {c for c in list_of_coords}
+
     if reduction_factor > 9:
         raise Exception("max reduction factor is 9")
 
@@ -23,7 +25,7 @@ def _lower_resolution_v2(list_of_coords, reduction_factor):
     result.append(list_of_coords[0])
 
     for coord in list_of_coords:
-        if int(str(coord[0])[-1]) < reduction_factor:
+        if int(str(coord[0])[-1]) < reduction_factor and coord not in corners:
             continue
         result.append(coord)
 
